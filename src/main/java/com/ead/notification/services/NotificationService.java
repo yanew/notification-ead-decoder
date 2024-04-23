dc80b3e8a761bc0ec9c81e524ed44c7c569ea5c9
Yane Wanderley <yane.rodrigues@ufpe.br>
2024-04-23 00:18:11 -0300

    Criar Exchange Topic, definir Queue e Bindings

--- src/main/java/com/ead/notification/services/NotificationService.java
+++ src/main/java/com/ead/notification/services/NotificationService.java
@@ -1,5 +1,9 @@
 package com.ead.notification.services;
 
+import com.ead.notification.models.NotificationModel;
+
 public interface NotificationService {
+
+	public NotificationModel saveNotification(NotificationModel notificationModel);
 
 }
--- src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
+++ src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
@@ -2,6 +2,7 @@ package com.ead.notification.services.impl;
 
 import org.springframework.stereotype.Service;
 
+import com.ead.notification.models.NotificationModel;
 import com.ead.notification.repositories.NotificationRepository;
 import com.ead.notification.services.NotificationService;
 
@@ -12,6 +13,11 @@ public class NotificationServiceImpl implements NotificationService{
 	
 	public NotificationServiceImpl(NotificationRepository notificationRepository) {
 		this.notificationRepository = notificationRepository;
+	}
+
+	@Override
+	public NotificationModel saveNotification(NotificationModel notificationModel) {
+		return notificationRepository.save(notificationModel);
 	}
 	
 }
